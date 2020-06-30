e755819c205ac54356dfc812b50b1ed050a20d07
said-droid <said.elkharroubi@hotmail.com>
2020-06-30 16:44:50 +0200

    new layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('run').addEventListener('click', ()=>{
+/*document.getElementById('run').addEventListener('click', ()=>{
 
     let city = document.getElementById('city').value;
 
@@ -6,5 +6,24 @@ document.getElementById('run').addEventListener('click', ()=>{
     .then(data => data.json())
     .then(weather => console.log(weather))
     .catch(error => console.error(error));
-});
+});*/
+
+const api = {
+    url: 'https://api.openweathermap.org/data/2.5/forecast?q=',
+    apiKey: '&appid=ce24f6ae35615d5b8208834c2524de00',
+    unit: '&units=metric'
+}
+
+const input = document.querySelector('.inputfield');
+input.addEventListener('keypress', setQuery);
+
+function setquery(evt){
+    if(evt.keyCode === 13){
+        getResults(input.value);
+    }
+}
+
+function getResults(query){
+    fetch()
+}
 
